d1fddd71a32f4260be899f750c1fd43b1ea68988
Tomáš Knap <tomasknap007@gmail.com>
2025-02-23 18:15:24 +0100

    AddedNotes

--- Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff53eb985baf4bc86a946f1bf5dc9dcb2ed7540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c629f7160bb52dbd1b6c660e45dafce4048deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
