6d5ebff8649d14a9da803a2bc55c3de9852c2f72
Tomáš Knap <tomasknap007@gmail.com>
2025-03-01 16:27:07 +0100

    AddedPlaceholders

---
+++ Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Repository<Product> products;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            products = new Repository<Product>(context);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await products.GetAllAsync());
+        }
+    }
+}
